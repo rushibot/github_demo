2d8bc83a9f3fbaa7e5e15ba819e394fb8e6aec8c
rushi <rushikesh.tapdiya47@gmail.com>
2021-07-03 13:27:31 +0530

    Implementation of add and substract

--- math.py
+++ math.py
@@ -1,9 +1,11 @@
 # Add implementation
 def add(x,y):
     return x+y
+    #add implementation
 # Subtract implementation
 def subtract(x,y):
     return x-y
+     #substract implementation
 # Multiply implementation
 def multiply(x,y):
     return x*y   #on master branch
