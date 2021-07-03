c58ca0ecc185fead09f33f5416977e3ab96378b9
rushibot <51736214+rushibot@users.noreply.github.com>
2021-07-03 13:21:01 +0530

    Added math.py to the remote repo

---
+++ math.py
@@ -0,0 +1,19 @@
+# Add implementation
+def add(x,y):
+    return x+y
+# Subtract implementation
+def subtract(x,y):
+    return x-y
+# Multiply implementation
+def multiply(x,y):
+    return x*y   #on master branch
+# Divide implementation
+def divide(x,y):
+<<<<<<< HEAD
+    return x/y #on master branch
+=======
+    if (x==0): #on Bug789 branch
+      return x/y
+    else:
+       return "Divide by zero error"
+
